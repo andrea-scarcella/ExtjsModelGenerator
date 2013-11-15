7d9f74b1bce7e6a821491ff3a5c036d4c584640c
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-11-15 02:32:12 +0100

    Temporary commit

---
+++ ExtModelGenerator/ExtModelGenerator.Test/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ExtModelGenerator.Test
+{
+	[TestFixture]
+    public class Class1
+    {
+		public string fileName { get; set; }
+		public Assembly assembly { get; set; }
+
+		[TestFixtureSetUp]
+		public void MyTestMethod()
+		{
+			fileName = "";
+			 assembly = Assembly.ReflectionOnlyLoadFrom(fileName);	
+		}
+		[SetUp]
+		public void Setup() {
+			
+
+		}
+
+
+
+		
+	}
+}
